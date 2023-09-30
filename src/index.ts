a723f547a5cb0a668d39ec4ad80d0c608bb39b99
rezonmain <adiazgomez101@gmail.com>
2023-09-29 20:50:30 -0700

    feat(app): test index

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ import { TwitchCollector } from "./services/TwitchCollector";
 
 const twitchCollector = new TwitchCollector("paymoneywubby");
 twitchCollector.start();
+
+setInterval(() => {
+  console.clear();
+  console.log(twitchCollector.stats);
+}, 5000);
