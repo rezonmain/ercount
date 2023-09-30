5449df131939d21353c276be9b01ab365dc2300f
rezonmain <adiazgomez101@gmail.com>
2023-09-30 02:58:11 -0700

    feat(analytics): add calculate method

--- src/services/Analytics.ts
+++ src/services/Analytics.ts
@@ -1,3 +1,6 @@
+import type { LoggerSuite } from "@/types/logger.types";
+import { Helpers } from "./Helpers";
+
 class Analytics {
   private _durationSeconds = 0;
   private _viewCount: Record<"max" | "min" | "avg", number> = {
@@ -11,9 +14,33 @@ class Analytics {
   };
   private _messages: Record<"total", number> = { total: 0 };
   private _tally?: Record<string, number> = {};
-  private done = false;
+  private loggers?: LoggerSuite;
+  done = false;
 
-  constructor(private baseFilePath: string) {}
+  constructor(private base: string = "default") {}
+
+  async calculate(): Promise<void> {
+    this.loggers = Helpers.logger.getAllLoggers(this.base);
+
+    const { logTimes } = await this.loggers.meta.parseLogFile();
+    console.log(logTimes);
+    const viewCountLogs = await this.loggers.views.parseLogFile();
+    const chatterMessages = await this.loggers.chatters.parseLogFile();
+
+    const durationSeconds = Helpers.time.getDurationSeconds(logTimes);
+
+    const tally = Helpers.analytics.getTally(chatterMessages);
+    const messagesCount = { total: chatterMessages.length };
+    const viewCount = Helpers.analytics.getViewCount(viewCountLogs);
+    const chattersCount = Helpers.analytics.getChattersCount(chatterMessages);
+
+    this._durationSeconds = durationSeconds;
+    this._viewCount = viewCount;
+    this._chatters = chattersCount;
+    this._messages = messagesCount;
+    this._tally = tally;
+    this.done = true;
+  }
 
   get durationSeconds() {
     return this._durationSeconds;
