bd938cd90c1178424fb159c3833b9700811fe625
rezonmain <adiazgomez101@gmail.com>
2023-09-29 21:40:28 -0700

    feat(index): handle not live error, listen to stop message on cli

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import { NotLiveError } from "./interfaces/Errors";
 import { TwitchCollector } from "./services/TwitchCollector";
 
 const twitchCollector = new TwitchCollector("paymoneywubby");
-twitchCollector.start();
 
-setInterval(() => {
-  console.clear();
-  console.log(twitchCollector.stats);
-}, 5000);
+try {
+  await twitchCollector.start();
+} catch (e) {
+  if (e instanceof NotLiveError) {
+    console.error(e.message);
+    process.exit(0);
+  }
+}
+
+for await (const line of console) {
+  if (line === "stop") {
+    twitchCollector.stop();
+    break;
+  }
+}
