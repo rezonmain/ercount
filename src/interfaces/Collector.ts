d2e6e621a1bfa5a506e3bb494ff7c62615ff7f06
rezonmain <adiazgomez101@gmail.com>
2023-09-29 19:31:22 -0700

    feat(Collector): update stop, add meta logger

--- src/interfaces/Collector.ts
+++ src/interfaces/Collector.ts
@@ -6,26 +6,38 @@ import {
 import { ChattersLogger } from "@/services/ChattersLogger";
 import { OutLogger } from "@/services/OutLogger";
 import { ViewsLogger } from "@/services/ViewsLogger";
+import { MetaLogger } from "@/services/MetaLogger";
 
 abstract class Collector {
   private OUT_FILE_DIR = "./data";
   private CHATTER_LOG_EXT = "chatters.log";
   private VIEW_COUNT_LOG_EXT = "views.log";
   private OUT_FILE_EXT = "json";
+  private META_FILE_EXT = "meta.json";
   private VIEW_COUNT_SAMPLE_RATE = 1000 * 60 * 5; // 5 minutes
   protected logTimes: TimesLog = { init: new Date(), end: new Date() };
   protected loggers?: {
     out: OutLogger;
     chatters: ChattersLogger;
     views: ViewsLogger;
+    meta: MetaLogger;
+  };
+  stats = {
+    viewerCount: 0,
+    streamTitle: "",
+    lastMessage: {
+      displayName: "",
+      message: "",
+      ts: "",
+    },
   };
 
   constructor(protected channelName: string) {}
 
   abstract _start(): void;
+  abstract _stop(): void;
   abstract getCurrentViewerCount(): Promise<ViewCountLog>;
-  abstract onChatterMessage(chatter: ChatterMessageLog): void;
-  abstract getStreamTitle(): Promise<number>;
+  abstract getStreamTitle(): Promise<string>;
 
   start(): void {
     this.logTimes.init = new Date();
@@ -38,6 +50,7 @@ abstract class Collector {
       out: new OutLogger(logFilePaths.out),
       chatters: new ChattersLogger(logFilePaths.chatters),
       views: new ViewsLogger(logFilePaths.views),
+      meta: new MetaLogger(logFilePaths.out),
     };
 
     this._start();
@@ -46,22 +59,25 @@ abstract class Collector {
   async stop() {
     this.logTimes.end = new Date();
     this.loggers?.out.log({
-      fileMeta: {
-        channelName: this.channelName,
-        logTimes: this.logTimes,
-        viewerCountSampleIntervalMs: this.VIEW_COUNT_SAMPLE_RATE,
-      },
+      fileMeta: await this.loggers.meta.parseLogFile(),
       viewerCounts: await this.loggers.views.parseLogFile(),
       chatters: await this.loggers.chatters.parseLogFile(),
     });
     Object.values(this.loggers!).forEach((logger) => logger.end());
+    this._stop();
   }
 
   private getLogFilePaths = ({ base }: { base: string }) => ({
     out: `${this.OUT_FILE_DIR}/${base}.${this.OUT_FILE_EXT}`,
     chatters: `${this.OUT_FILE_DIR}/${base}.${this.CHATTER_LOG_EXT}`,
     views: `${this.OUT_FILE_DIR}/${base}.${this.VIEW_COUNT_LOG_EXT}`,
+    meta: `${this.OUT_FILE_DIR}/${base}.${this.META_FILE_EXT}`,
   });
+
+  protected onChatterMessage(chatter: ChatterMessageLog): void {
+    this.loggers?.chatters.log(chatter);
+    this.stats.lastMessage = chatter;
+  }
 }
 
 export { Collector };
