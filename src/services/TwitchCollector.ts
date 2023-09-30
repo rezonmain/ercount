a08ca1d71a0df939f47ba25fc4dd24692ef3fe1a
rezonmain <adiazgomez101@gmail.com>
2023-09-29 19:33:04 -0700

    feat(TwitchCollector): implement interface methods

--- src/services/TwitchCollector.ts
+++ src/services/TwitchCollector.ts
@@ -1,16 +1,49 @@
+import { ViewCountLog } from "@/types/logger.types.js";
 import { Collector } from "../interfaces/Collector.js";
+import tmi from "tmi.js";
+import { TwitchAPI } from "./TwitchAPI.js";
 
 class TwitchCollector extends Collector {
+  private chat;
+  private api;
   constructor(channelName: string) {
     super(channelName);
+    this.chat = new tmi.Client({
+      channels: [channelName],
+    });
+    this.api = new TwitchAPI();
   }
 
-  start(): void {
-    console.log(`Starting Twitch collector for ${this.channelName}`);
+  _start(): void {
+    this.chat.connect();
+    this.registerChatListeners();
   }
 
-  stop(): void {
-    console.log(`Stopping Twitch collector for ${this.channelName}`);
+  _stop(): void {
+    this.chat.disconnect();
+  }
+
+  async getCurrentViewerCount(): Promise<ViewCountLog> {
+    const [viewerCount] = await this.api.getStreams(this.channelName);
+    return {
+      ts: new Date().toISOString(),
+      count: viewerCount.viewer_count,
+    };
+  }
+
+  async getStreamTitle(): Promise<string> {
+    const [streamTitle] = await this.api.getStreams(this.channelName);
+    return streamTitle.title;
+  }
+
+  private registerChatListeners() {
+    this.chat.on("message", (channel, tags, message) => {
+      this.onChatterMessage({
+        displayName: tags["display-name"] ?? "anonymous",
+        message,
+        ts: new Date().toISOString(),
+      });
+    });
   }
 }
 
