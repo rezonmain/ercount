8161f798d445d8556ce4c51270300bccf23d6e1d
rezonmain <adiazgomez101@gmail.com>
2023-10-01 18:16:47 -0700

    feat(index): log error

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,3 @@
-import { NotLiveError } from "./interfaces/Errors";
-import { TwitchCollector } from "./services/TwitchCollector";
-
-const channelName = process.argv[2];
-const twitchCollector = new TwitchCollector(channelName);
-
-try {
-  await twitchCollector.start();
-} catch (e) {
-  if (e instanceof NotLiveError) {
-    console.error(e.message);
-    process.exit(0);
-  }
-}
-
-process.on("SIGINT", async () => {
-  await twitchCollector.stop();
-  process.exit(0);
-});
-
-for await (const line of console) {
-  if (line === "stop") {
-    await twitchCollector.stop();
-    break;
-  }
-}
-
-await twitchCollector.analytics.log();
+console.error("Run log script instead");
+console.info("Usage: bun log <channel name>");
+process.exit(0);
