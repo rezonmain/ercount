c5141995c608dcb7954e9489a7c4f79ba7c0a21f
rezonmain <adiazgomez101@gmail.com>
2023-10-01 21:46:12 -0700

    feat(analyzeScript): use Debug

--- src/scripts/analyze.ts
+++ src/scripts/analyze.ts
@@ -1,15 +1,16 @@
 import { Analytics } from "@/services/Analytics";
+import { Debug } from "@/services/Debug";
 import { Helpers } from "@/services/Helpers";
 
 const base = process.argv[2];
 
 if (!base) {
-  console.error("No base provided");
+  Debug.error("No base provided");
   process.exit(1);
 }
 
 if (!(await Helpers.file.exists(base + ".meta.json"))) {
-  console.error("No meta file found for the provided base");
+  Debug.error("No meta file found for the provided base");
   process.exit(1);
 }
 
